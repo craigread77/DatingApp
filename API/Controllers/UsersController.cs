99bc99d62283bb4efd8342bb3cba874dfb90a5ae
Craig Read <craigread7@hotmail.co.uk>
2024-07-31 21:31:07 +0100

    commit message

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+
+        var users = await context.Users.ToListAsync();
+
+        return users;
+        
+    }
+
+    [HttpGet("{id:int}")] // /api/users/{id}
+    public async Task<ActionResult<AppUser>> GetUser(int id) {
+
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+
+    [HttpGet("username/{username}")] // /api/users/username/{username}
+    public async Task<ActionResult<AppUser>> GetUserByUserName(string username) {
+
+        var user = await context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
